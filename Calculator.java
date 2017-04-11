1b0508471cd2aa772ab12296f39332fcf4b67732
TejaswiChava <teja.chava4@gmail.com>
2017-04-11 09:51:54 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+package info;
+
+public class Calculator {
+
+
+public int substract(Integer a,Integer b)
+{
+	
+	return a-b;
+	
+}
+public int multiplication(int a,int b)
+{
+	return a*b;
+}
+public int division(int a,int b)
+{
+	return (a/b);
+}
+public int modulus(int a,int b)
+{
+	return a%b;
+	
+}
+public int add(Integer a, Integer b) {
+	
+	return a+b;
+}
+
+
+
+}
+	
+
